f20ed13a7e0debfc129735e4cba54287c56be946
Ben Manley <benjamintmanley@gmail.com>
2023-09-21 14:33:27 -0500

    implement profile show functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,7 +14,24 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.profileId)
+  .then(profile => {
+    const isSelf = profile._id.equals(req.user.profile._id)
+    res.render('profiles/show', {
+      title: `😺 ${profile.name}'s profile`,
+      profile,
+      isSelf
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/profiles')
+  })
+}
+
 export {
   index,
+  show,
 
 }
