529a4fffbb8d43624985ee81ddf61049a9d7ab13
jurgenstevens <jurgen.stevens@yahoo.com>
2024-01-17 14:07:19 -0600

    Complete flip-tasty functionality.

--- routes/tacos.js
+++ routes/tacos.js
@@ -6,8 +6,12 @@ const router = Router()
 
 // GET localhost:3000/tacos
 router.get('/', tacosCtrl.index)
+// GET localhost:3000/tacos
+router.get('/:tacoId', tacosCtrl.show)
 // POST localhost:3000/tacos
 router.post('/', isLoggedIn, tacosCtrl.create)
+// PATCH localhost:3000/tacos/:tacoId/flip-tasty
+router.patch('/:tacoId/flip-tasty', isLoggedIn, tacosCtrl.flipTasty)
 
 export {
   router
