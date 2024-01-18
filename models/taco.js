370844263c1b7aba7eea80d33ca0ee7ccc96b7ec
Ben Manley <benjamintmanley@gmail.com>
2024-01-18 14:54:11 -0600

    add comment form for taco

--- models/taco.js
+++ models/taco.js
@@ -2,10 +2,18 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  content: String,
+  author: { type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
 const tacoSchema = new Schema({
   name: String,
   tasty: Boolean,
   owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  comments: [commentSchema]
 }, {
   timestamps: true,
 })
