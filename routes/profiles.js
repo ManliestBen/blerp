eb597002c182402f200102a1d71711aec7a0938f
Ben Manley <benjamintmanley@gmail.com>
2024-01-19 10:53:41 -0600

    implement show controller for profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,10 @@ import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router()
 
+// GET http://localhost:3000/profiles
 router.get('/', isLoggedIn, profilesCtrl.index)
+// GET http://localhost:3000/profiles/:profileId
+router.get('/:profileId', isLoggedIn, profilesCtrl.show)
 
 export {
   router
